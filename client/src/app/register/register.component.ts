ffde68845ad0ea74cdf57b49f9e43d5cb4b129a0
Vineet Shah <brake.vin@gmail.com>
2022-06-02 18:58:49 +0530

    End of section 6

--- client/src/app/register/register.component.ts
+++ client/src/app/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { ToastrService } from 'ngx-toastr';
 import { AccountService } from '../_services/account.service';
 
 @Component({
@@ -11,7 +12,7 @@ export class RegisterComponent implements OnInit {
   @Output() cancelRegister = new EventEmitter();
   model: any = {};
 
-  constructor(private accountService: AccountService) { }
+  constructor(private accountService: AccountService, private toastr:ToastrService) { }
 
   ngOnInit(): void {
   }
@@ -20,6 +21,9 @@ export class RegisterComponent implements OnInit {
     this.accountService.register(this.model).subscribe(response => {
       console.log(response);
       this.cancel();
+    }, error => {
+      console.log(error);
+      this.toastr.error(error.error);
     })
     console.log(this.model);
   }
